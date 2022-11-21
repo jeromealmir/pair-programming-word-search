63baf3b9420c53d100cd08601b97f05ce54cd889
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-20 21:19:50 -0800

    add support for vertical search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+    
+  for (const l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+    
+  const vertical = [];
 
-module.exports = wordSearch
+  for (let row = 0; row < letters[0].length; row++) {
+    let found = [];
+    for (let column = 0; column < letters.length; column++) {
+      found.push(letters[column][row]);
+    }
+    vertical.push(found.join(''));
+  }
+
+  for (const v of vertical) {
+    if (v.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
